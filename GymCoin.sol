68e1430c6a12112996fcc3665512b89585ac9d8c
Mingxuan225Wu <56039253+Mingxuan225Wu@users.noreply.github.com>
2022-01-12 14:57:51 -0800

    GymCoin combined with Smart Device, could deploy

--- GymCoin.sol
+++ GymCoin.sol
@@ -3,37 +3,49 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract GymCoin {
-   
+
+    struct exercise {
+        bytes32 userID;   // short name (up to 32 bytes)
+        uint deviceID;    // the device ID for user
+        uint gymcoin;    //gym coin amount
+    }
+
     struct User {
         bytes32 userID; // Unique ID for each user
-        bytes32[] deviceID; // Unique ID for each device
-        // character;
+        uint [] deviceIDs; // Unique ID for each device
         uint balance; // Balance of GymCoins
-        // transaction;
-        bytes32[] contractID; // List of contracts signed
+        uint contractID; // List of contracts signed
     }
 
     mapping(address => User) public Users;      //TODO: after mapping address to User, what does Users do?
-    User [] UserList;
-    bytes32 [] userIDs;
 
-    constructor(bytes32 userid, bytes32[] memory deviceIDs, uint balance, bytes32[] memory contractIDs) {
-        UserList.push(User({
-            userID: userid,         //TODO: don't we technically just need user's address to transact to make it actually on a blockchain?
-            deviceID: deviceIDs,
-            balance: balance,
-            contractID: contractIDs
-            }));
-        userIDs.push(userid);
+    function add_user(bytes32 userid, uint [] memory deviceIDs, uint balance, uint contractIDs) public {
+        User storage sender = Users[msg.sender];
+        sender.userID = userid;
+        sender.deviceIDs = deviceIDs;
+        sender.balance = balance;
+        sender.contractID = contractIDs;
     }
 
 
-    // function exchange(bytes32 user2, uint amount) public {
-    //     User storage sender = Users[msg.sender];
-    //     // require(!sender.voted, "Already voted.");
-    //     sender.balance -= amount; // Could be negative
-    //     userIDs[user2];
-    // }
+    function add_exercise(bytes32 userid, uint deviceid, uint heartrate1, uint workout_type1, uint workout_time1, uint calories1) public {
+        User storage sender = Users[msg.sender];
+        // require(deviceid in sender.deviceIDs);
+        sender.balance += reward(heartrate1,workout_type1,workout_time1,calories1);
+    }
+
+    function reward(uint heartrate1, uint workout_type1, uint workout_time1, uint calories1) public view returns(uint earned){
+        //The formula will divide by the heartrate to make it fair to those people who do weight training
+        earned = calories1 / heartrate1 * workout_time1 / 2000;
+    }
+
+    function exchange(address user2, uint amount) public {
+        User storage sender = Users[msg.sender];
+        require(sender.balance >= amount);
+        
+        sender.balance -= amount; // Could be negative
+        Users[user2].balance += amount;
+    }
 
     function signContract(bytes32 userID, bytes32 contractID) public {
 
@@ -46,26 +58,27 @@ contract GymCoin {
 }
 
 
-contract token { 
-    mapping (uint => mapping (address => uint)) coinBalanceOf;
-    event CoinTransfer(uint coinType, address sender, address receiver, uint amount);
+// contract token { 
+//     mapping (uint => mapping (address => uint)) coinBalanceOf;
 
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function tokenSupply(uint numCoinTypes, uint supply) public { //
-        for (uint k=0; k<numCoinTypes; ++k) {
-            coinBalanceOf[k][msg.sender] = supply;
-        }
-    }
+//     event CoinTransfer(uint coinType, address sender, address receiver, uint amount);
 
-    /* Very simple trade function */
-    function sendCoin(uint coinType, address receiver, uint amount) public returns (bool sufficient) {
-        if (coinBalanceOf[coinType][msg.sender] < amount) return false;
+//     /* Initializes contract with initial supply tokens to the creator of the contract */
+//     function tokenSupply(uint numCoinTypes, uint supply) public { //
+//         for (uint k=0; k<numCoinTypes; ++k) {
+//             coinBalanceOf[k][msg.sender] = supply;
+//         }
+//     }
 
-        coinBalanceOf[coinType][msg.sender] -= amount;
-        coinBalanceOf[coinType][receiver] += amount;
+//     /* Very simple trade function */
+//     function sendCoin(uint coinType, address receiver, uint amount) public returns (bool sufficient) {
+//         if (coinBalanceOf[coinType][msg.sender] < amount) return false;
 
-        emit CoinTransfer(coinType, msg.sender, receiver, amount);
+//         coinBalanceOf[coinType][msg.sender] -= amount;
+//         coinBalanceOf[coinType][receiver] += amount;
 
-        return true;
-    }
-}
+//         emit CoinTransfer(coinType, msg.sender, receiver, amount);
+
+//         return true;
+//     }
+// }
