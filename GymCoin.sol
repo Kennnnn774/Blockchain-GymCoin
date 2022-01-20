e8d6b67377f510cbe862729fb357cf4e30a27d54
Mingxuan225Wu <56039253+Mingxuan225Wu@users.noreply.github.com>
2022-01-20 14:39:52 -0800

    Update time and functionality

--- GymCoin.sol
+++ GymCoin.sol
@@ -2,50 +2,62 @@
 
 pragma solidity >=0.7.0 <0.9.0;
 
+// import "./Datetime.sol";
+// import moment from "moment";
+
+
 contract GymCoin {
 
     struct Exercise {
-        bytes32 userID;   // short name (up to 32 bytes)
-        uint deviceID;    // the device ID for user
-        uint gymcoin;    //gym coin amount
+        bytes32 userID;   // Short name (up to 32 bytes)
+        uint deviceID;    // The device ID for user
+        uint gymcoin;    // GymCoin amount
     }
-
+    
     struct User {
         bytes32 userID; // Unique ID for each user
         uint [] deviceIDs; // Unique ID for each device
         uint balance; // Balance of GymCoins
+        uint balance_reset; // Keep track of total balance at the previous round
         uint contractID; // List of contracts signed
         bool registered; // true means one has already registered and cannot add_user again
+        uint timeSigned; // The baseline time of signing one Contract
+        // DateTime time;
     }
 
-    // struct Platform {
-    //     bytes32 [] userIDs; // Unique ID for each user 
-    //     Post [] Posts;
-    // }
-
-    struct Post {
+    struct Post { // Could also do Like System
+        // uint postID;
         bytes32 userID; // Unique ID for each user
         string context;
-        string time;
+        uint time;
+        // uint numberOfLikes;
     }
 
-    mapping(address => User) public Users;      //TODO: after mapping address to User, what does Users do?
+    mapping(address => User) public Users;
     Post[] public Posts;
 
-
-    function add_user(bytes32 userid, uint [] memory deviceIDs, uint contractIDs) public {
+    function Register(bytes32 userid, uint [] memory deviceIDs) public {
         User storage sender = Users[msg.sender];
         require(!sender.registered, "You have already registered");
 
         sender.userID = userid;
         sender.deviceIDs = deviceIDs;
         sender.balance = 0;
-        sender.contractID = contractIDs;
+        sender.balance_reset = 0;
+        sender.contractID = 0;
         sender.registered = true;
+    }
+
+    function add_device(uint newDeviceID) public {
+        User storage sender = Users[msg.sender];
+        require(sender.registered == true, "You have not registered.");
+
+        sender.deviceIDs.push(newDeviceID);
     }
 
     function add_exercise(bytes32 userid, uint deviceid, uint heartrate1, uint workout_time1, uint calories1) public {
         User storage sender = Users[msg.sender];
+        require(sender.registered == true, "You have not registered.");
         require(sender.userID == userid, "Wrong User ID");
 
         // Check if the device is recorded
@@ -58,37 +70,49 @@ contract GymCoin {
         sender.balance += reward(heartrate1, workout_time1, calories1);
     }
 
-
-    function reward(uint heartrate1, uint workout_time1, uint calories1) public pure returns(uint earned){
+    function reward(uint heartrate1, uint workout_time1, uint calories1) private pure returns(uint earned){
         //The formula will divide by the heartrate to make it fair to those people who do weight training
         earned = calories1 / heartrate1 * workout_time1 / 2000;
     }
 
+// 0x0000000000000000000000000000000000000000000000000000000061626364
+// [1, 2, 3]
+// 0x0000000000000000000000000000000000000000000000000000000061626365
 
     function exchange(address user2, uint amount) public {
         User storage sender = Users[msg.sender];
+        require(sender.registered == true, "You have not registered.");
+
         require(sender.balance >= amount);
+        require(amount >= 0);
         
-        sender.balance -= amount; // Could be negative
+        sender.balance -= amount;
         Users[user2].balance += amount;
     }
 
-    function add_post(bytes32 userid, string memory context, string memory time) public {
-        // Post storage sender = Posts[msg.sender];
+    function add_post(bytes32 userid, string memory context, uint time) public {
         User storage user_sender = Users[msg.sender];
+        require(user_sender.registered == true, "You have not registered.");
         require(user_sender.userID == userid, "You must post for yourself");
-        // sender.userID = userid;
-        // sender.context = context;
-        // sender.time = time;
 
         Posts.push( Post(userid, context, time) );
     }
 
-    function watchFivePosts(bytes32 userid) public view returns (string[] memory, string[] memory) {
-        string [] memory curr_posts = new string [](5);
-        string [] memory curr_posts_time = new string [](5);
 
-        for (uint i=0; i < Posts.length; i++) {
+// 0x0000000000000000000000000000000000000000000000000000000061626364
+// [1, 2, 3]
+// 0x0000000000000000000000000000000000000000000000000000000061626365
+
+
+    function watchFivePosts(bytes32 userid) public view returns (string[] memory, uint[] memory) {
+        User storage sender = Users[msg.sender];
+        require(sender.registered == true, "You have not registered.");
+
+        string [] memory curr_posts = new string [](5);
+        uint [] memory curr_posts_time = new uint [](5);
+
+        // try catch
+        for (uint i = Posts.length-1; i > 0; i--) {
             if (userid == Posts[i].userID) {
                 curr_posts[i] = Posts[i].context;
                 curr_posts_time[i] = Posts[i].time;
@@ -97,14 +121,47 @@ contract GymCoin {
         return (curr_posts, curr_posts_time);
     }
 
-    // function signContract(bytes32 userID, bytes32 contractID) public {
 
-    // }
+    function signContract(uint contractID) public {
+        User storage sender = Users[msg.sender];
+        require(sender.registered == true, "You have not registered.");
+        require(sender.contractID == 0, "You've already signed a contract. Please cancel it before you sign a new one.");
 
-    // function cancelContract(bytes32 userID, bytes32 contractID) public {
+        sender.contractID = contractID;
 
-    // }
-    
+        sender.timeSigned = block.timestamp;
+        // sender.time = DateTime();
+    }
+
+    function cancelContract() public {
+        User storage sender = Users[msg.sender];
+        require(sender.registered == true, "You have not registered.");
+
+        sender.contractID = 0;
+    }
+
+    function commitContract() public {
+        User storage sender = Users[msg.sender];
+        uint curr_time = block.timestamp - sender.timeSigned;
+
+        if (sender.contractID == 100) {
+            if (curr_time >= 1 weeks && curr_time <= 1 weeks + 1 days) {
+                if (sender.balance - sender.balance_reset >= 25){ sender.balance += 5;} 
+                else { sender.balance -= 3;} 
+            }
+        } if (sender.contractID == 200) {
+            if (curr_time >= 1 weeks && curr_time <= 1 weeks + 1 days) {
+                if (sender.balance - sender.balance_reset >= 30){ sender.balance += 7;} 
+                else { sender.balance -= 4;} 
+            }
+        } if (sender.contractID == 300) {
+            if (curr_time >= 1 weeks && curr_time <= 1 weeks + 1 days) {
+                if (sender.balance - sender.balance_reset >= 35){ sender.balance += 9;} 
+                else {sender.balance -= 5;} 
+            }
+        }  
+        sender.balance_reset = sender.balance;
+    }
 }
 
 
